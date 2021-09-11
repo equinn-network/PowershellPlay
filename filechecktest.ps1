6480ec1e2e115bcccde22b7ffc7aedaa8d61bead
Eoin <eq@eoinquinn.ie>
2021-09-11 10:44:22 +0100

    Filecheck fix

--- filechecktest.ps1
+++ filechecktest.ps1
@@ -1,13 +1,13 @@
+# Set Directory to Check for files
 $dir = "D:/Code/Test"
-
-$fileCheck = Get-ChildItem -Path $dir -Recurse | where {([datetime]::now.Date -eq $_.lastwritetime.Date)};
-
-$fileTest = Test-Path $fileCheck.FullName
-
-if ($fileTest -eq $true ) {
-
-Write-Host "Backup Successful. $fileCheck is todays backup."
+# Use Child Item and datetime to identify file. Item is put in array using @() to do a true/false check.
+$fileCheck = @(Get-ChildItem -Path $dir -Include Backup*.bak -Recurse | where {([datetime]::now.Date -eq $_.lastwritetime.Date)};)
+# Check if File Check array has items
+if ($fileCheck.Length -eq 0 ) {
+# if no files write failure
+Write-Host "FAILURE! No Backup File available from today."
         }
+# If successful write success message
 else {
-    Write-Host "FAILURE! No Backup File available from today."
+    Write-Host "Success! $fileCheck is todays backup file"
 }
