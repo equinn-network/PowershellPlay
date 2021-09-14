c46dc54ac239213cc3be98e27b505e9bccc66a94
Eoin <eq@eoinquinn.ie>
2021-09-14 22:36:57 +0100

    install posh-ssh

---
+++ PS_SSH_Test.ps1
@@ -0,0 +1,4 @@
+# Requires Posh-SSH Module. Can be installed from Powrshell Gallery using command Import-Module PowerShellGet & Install-Module Posh-SSH
+# May need a change in Execution policy using Set-ExecutionPolicy Unrestricted
+
+New-SSHSession 
