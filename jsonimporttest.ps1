2a374d18a55a8007b797393dbcf22689f8716b8d
Eoin <eq@eoinquinn.ie>
2021-09-16 21:21:16 +0100

    Playing with sums

--- jsonimporttest.ps1
+++ jsonimporttest.ps1
@@ -1,12 +1,35 @@
-$results = Invoke-WebRequest -Uri https://jsonplaceholder.typicode.com/comments | ConvertFrom-Json
+# Test 1
+#$results = Invoke-WebRequest -Uri https://jsonplaceholder.typicode.com/comments | ConvertFrom-Json
 
 #foreach($result in $results)
 #{
 #Write-Host "$($result.name) has the email $($result.email)"
 #}
 
-$user = $results | ? {$_.email -match 'Lew@alysha.tv' }
+#$user = $results | ? {$_.email -match 'Lew@alysha.tv' }
 
-$userName = $user.name
+#$userName = $user.name
 
-Write-Host = $userName
+#Write-Host = $userName
+
+#
+# Test 2
+
+$results = Invoke-WebRequest -uri https://jsonplaceholder.typicode.com/users | ConvertFrom-Json
+
+$ids = $results.id 
+
+$discardTotal = 0
+
+$ids | ForEach {$discardTotal += $_}
+
+if($discardTotal -eq 55)
+            {
+                Write-Host " $discardTotal . Looks Good!"
+            }
+else
+            {
+
+                Write-Host "Result. '$discardTotal'.Low. Didn't add right"
+
+            }
