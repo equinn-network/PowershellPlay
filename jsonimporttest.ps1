f160842c22cca6faadf5369141b46134ed417c6f
Eoin <eq@eoinquinn.ie>
2021-09-16 21:49:14 +0100

    Total sum

--- jsonimporttest.ps1
+++ jsonimporttest.ps1
@@ -14,22 +14,38 @@
 
 #
 # Test 2
-
+#Get Info
 $results = Invoke-WebRequest -uri https://jsonplaceholder.typicode.com/users | ConvertFrom-Json
-
+#Extract Required Fields
 $ids = $results.id 
-
+#Set total for sum
 $discardTotal = 0
-
+#Sum of each field from previous entry
 $ids | ForEach {$discardTotal += $_}
-
+# Print result
 if($discardTotal -eq 55)
             {
-                Write-Host " $discardTotal . Looks Good!"
+                Write-Host "$discardTotal. Looks Good!"
             }
 else
             {
 
-                Write-Host "Result. '$discardTotal'.Low. Didn't add right"
+                Write-Host "$discardTotal. Didn't add right."
 
             }
+# Sample variable for creating array
+$sampleVal = 10
+
+# Create array of results
+$allResults = $discardTotal,$sampleVal
+
+# Get final result
+$finalScore = ($allResults | measure-object -sum).Sum
+
+if($finalScore -eq 65)
+            {
+                Write-Host "$finalScore. Wooooooo!"
+            }
+else {
+        Write-Host "$finalScore. Something went wrong"
+}
